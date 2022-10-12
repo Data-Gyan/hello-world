58853947a4bfd11df1b41bc86eb353846bcae84c
Vivek Tyagi <vvktyagi@gmail.com>
2022-10-12 09:52:33 +0530

    Added test api

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ async def hello():
     return "(Hello-World..!  Welcome to first fastapi app.)"
 
 @app.get("/greetme/{name}")
-async def hello(name):
+async def greet(name):
     return "Dear {0}, You are very welcome here !".format(name)
+
+@app.get("/test/")
+async def test():
+    return "Testing Passed...!"
 
 
